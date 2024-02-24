d1c762e9675af60a4c4ff82ecada16f73d4c6217
AurélioMSFilho <aureliofilhotj@gmail.com>
2024-02-23 21:25:04 -0300

    primeiro commit

---
+++ Desafio Contador/src/desafioContador/Contador.java
@@ -0,0 +1,20 @@
+package desafioContador;
+
+import java.util.Scanner;
+
+public class Contador {
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		
+		System.out.println("Digite o primeiro número: ");
+		int parametro1 = terminal.nextInt();
+		System.out.println("Digite o segundo número: ");
+		int parametro2 = terminal.nextInt();
+		
+		ParametroInvalidosException parametro = new ParametroInvalidosException();
+		parametro.parametro(parametro1, parametro2);
+		
+		
+	}
+
+}
---
+++ Desafio Contador/src/desafioContador/ParametroInvalidosException.java
@@ -0,0 +1,23 @@
+package desafioContador;
+
+public class ParametroInvalidosException {
+	
+	public void parametro(int prm1, int prm2) {
+		try {
+			
+		
+		if (prm2 > prm1) {
+			int a = prm2 - prm1;
+			for (int i =1; i <= a; i++) {
+				System.out.println("Imprimindo o número "+i);
+			}
+		} else {
+			throw new Exception();
+		} 
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("O número dois deve ser maior que o número um");
+	}
+
+} 
+}
